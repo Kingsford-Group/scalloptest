d05d868205c2937a2c8cecabd02c4a5c023c860f
Mingfu Shao <shaomingfu@gmail.com>
2017-04-04 17:35:00 -0400

    add lists

--- bin/download.annotation.sh
+++ bin/download.annotation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ../data/ensembl
+
 wget ftp://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GRCh38.84.gtf.gz -O ../data/ensembl/GRCh38.gtf.gz
 wget ftp://ftp.ensembl.org/pub/grch37/release-87/gtf/homo_sapiens/Homo_sapiens.GRCh37.87.gtf.gz -O ../data/ensembl/GRCh37.gtf.gz
 
