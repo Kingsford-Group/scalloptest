809b8cb78778d1752856a8731484d4b6c2973613
Mingfu Shao <shaomingfu@gmail.com>
2017-04-04 18:22:40 -0400

    update collect

--- bin/collect.encode65.sh
+++ bin/collect.encode65.sh
@@ -26,12 +26,12 @@ do
 	ss=`echo $x | cut -f 2 -d ":"`
 	gm=`echo $x | cut -f 3 -d ":"`
 
-	x1=`cat $results/$id/scallop.$coverage/gffall.single | grep Matching | grep intron | grep chain | head -n 1 | awk '{print $4}'`
-	y1=`cat $results/$id/stringtie.$coverage/gffall.single | grep Matching | grep intron | grep chain | head -n 1 | awk '{print $4}'`
-	x2=`cat $results/$id/scallop.$coverage/gffall.single | grep Intron | grep chain | head -n 1 | awk '{print $6}'`
-	y2=`cat $results/$id/stringtie.$coverage/gffall.single | grep Intron | grep chain | head -n 1 | awk '{print $6}'`
-	z1=`cat $results/$id/transcomb.$coverage/gffall.single | grep Matching | grep intron | grep chain | head -n 1 | awk '{print $4}'`
-	z2=`cat $results/$id/transcomb.$coverage/gffall.single | grep Intron | grep chain | head -n 1 | awk '{print $6}'`
+	x1=`cat $results/$id/scallop.$coverage/gffmul.stats | grep Matching | grep intron | grep chain | head -n 1 | awk '{print $4}'`
+	y1=`cat $results/$id/stringtie.$coverage/gffmul.stats | grep Matching | grep intron | grep chain | head -n 1 | awk '{print $4}'`
+	x2=`cat $results/$id/scallop.$coverage/gffmul.stats | grep Intron | grep chain | head -n 1 | awk '{print $6}'`
+	y2=`cat $results/$id/stringtie.$coverage/gffmul.stats | grep Intron | grep chain | head -n 1 | awk '{print $6}'`
+	z1=`cat $results/$id/transcomb.$coverage/gffmul.stats | grep Matching | grep intron | grep chain | head -n 1 | awk '{print $4}'`
+	z2=`cat $results/$id/transcomb.$coverage/gffmul.stats | grep Intron | grep chain | head -n 1 | awk '{print $6}'`
 	echo "$id $x1 $x2 $y1 $y2 $z1 $z2"
 done
 
@@ -44,12 +44,12 @@ do
 	ss=`echo $x | cut -f 2 -d ":"`
 	gm=`echo $x | cut -f 3 -d ":"`
 
-	x1=`cat $results/$id.$aa/scallop.$coverage/gffall.scallop.gtf.tmap | awk '$6 == 1' | awk '$3 == "="' | wc -l`
-	x2=`cat $results/$id.$aa/scallop.$coverage/gffall.scallop.gtf.tmap | awk '$6 == 1' | wc -l`
-	y1=`cat $results/$id.$aa/stringtie.$coverage/gffall.stringtie.gtf.tmap | awk '$6 == 1' | awk '$3 == "="' | wc -l`
-	y2=`cat $results/$id.$aa/stringtie.$coverage/gffall.stringtie.gtf.tmap | awk '$6 == 1' | wc -l`
-	z1=`cat $results/$id.$aa/transcomb.$coverage/gffall.transcomb.gtf.tmap | awk '$6 == 1' | awk '$3 == "="' | wc -l`
-	z2=`cat $results/$id.$aa/transcomb.$coverage/gffall.transcomb.gtf.tmap | awk '$6 == 1' | wc -l`
+	x1=`cat $results/$id/scallop.$coverage/gffall.scallop.gtf.tmap | awk '$6 == 1' | awk '$3 == "="' | wc -l`
+	x2=`cat $results/$id/scallop.$coverage/gffall.scallop.gtf.tmap | awk '$6 == 1' | wc -l`
+	y1=`cat $results/$id/stringtie.$coverage/gffall.stringtie.gtf.tmap | awk '$6 == 1' | awk '$3 == "="' | wc -l`
+	y2=`cat $results/$id/stringtie.$coverage/gffall.stringtie.gtf.tmap | awk '$6 == 1' | wc -l`
+	z1=`cat $results/$id/transcomb.$coverage/gffall.transcomb.gtf.tmap | awk '$6 == 1' | awk '$3 == "="' | wc -l`
+	z2=`cat $results/$id/transcomb.$coverage/gffall.transcomb.gtf.tmap | awk '$6 == 1' | wc -l`
 
 	echo "$id $x1 $x2 $y1 $y2 $z1 $z2"
 done
